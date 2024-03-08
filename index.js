8e0dbd3759a60cbdd863d4b1b47d93421e99659d
Luvc098 <162357195+Luvc098@users.noreply.github.com>
2024-03-08 18:15:28 +0800

    index.js

--- index.js
+++ index.js
@@ -34,11 +34,7 @@ const r = new RichPresence()
 	.setURL('https://www.youtube.com/channel/UCWa_euD13oekqNsQPXYKXYw')
 	.setState('LiquidBounce')
 	.setName('LiquidBounce')
-	.setDetails('Halo')
-	.setParty({
-		max: 5,
-		current: 3,
-		id: getUUID(),
+	.setDetails('Halo'),
 	})
 	.setStartTimestamp(Date.now())
 	.setAssetsLargeImage('https://cdn.discordapp.com/attachments/1005727430675279942/1215589899936473128/637704298950098232.png?ex=65fd4d50&is=65ead850&hm=cb3a0aba5a7e0d36dadc461ca1802818b3340b84e644cb82772b21d1d7126f3f&')
